da99a251fd8d6a693844d4999f0af4e6a415bafd
hjeprua <122618482+Gino1216@users.noreply.github.com>
2025-02-20 10:14:08 +0700

    update nha cung cap

--- src/Controller/SearchNhaCungCap.java
+++ src/Controller/SearchNhaCungCap.java
@@ -4,13 +4,75 @@
  */
 package Controller;
 
+import HeThong.NhaCungCapDao;
+import Model.NhaCungCap;
 import java.util.ArrayList;
 
 /**
  *
- * @author Gino
+ * @author sinh
  */
 public class SearchNhaCungCap {
 
-   
+    public static SearchNhaCungCap getInstance() {
+        return new SearchNhaCungCap();
+    }
+
+    public ArrayList<NhaCungCap> searchTatCa(String text) {
+        ArrayList<NhaCungCap> result = new ArrayList<>();
+        ArrayList<NhaCungCap> armt = NhaCungCapDao.getInstance().selectAll();
+        for (var ncc : armt) {
+            if (ncc.getMaNhaCungCap().toLowerCase().contains(text.toLowerCase())
+                    || ncc.getTenNhaCungCap().toLowerCase().contains(text.toLowerCase())
+                    || ncc.getSdt().toLowerCase().contains(text.toLowerCase())
+                    || ncc.getDiaChi().toLowerCase().contains(text.toLowerCase())) {
+                result.add(ncc);
+            }
+        }
+        return result;
+    }
+
+    public ArrayList<NhaCungCap> searchTenNCC(String text) {
+        ArrayList<NhaCungCap> result = new ArrayList<>();
+        ArrayList<NhaCungCap> armt = NhaCungCapDao.getInstance().selectAll();
+        for (var ncc : armt) {
+            if (ncc.getTenNhaCungCap().toLowerCase().contains(text.toLowerCase())) {
+                result.add(ncc);
+            }
+        }
+        return result;
+    }
+
+    public ArrayList<NhaCungCap> searchMaNCC(String text) {
+        ArrayList<NhaCungCap> result = new ArrayList<>();
+        ArrayList<NhaCungCap> armt = NhaCungCapDao.getInstance().selectAll();
+        for (var ncc : armt) {
+            if (ncc.getMaNhaCungCap().toLowerCase().contains(text.toLowerCase())) {
+                result.add(ncc);
+            }
+        }
+        return result;
+    }
+
+    public ArrayList<NhaCungCap> searchDiaChi(String text) {
+        ArrayList<NhaCungCap> result = new ArrayList<>();
+        ArrayList<NhaCungCap> armt = NhaCungCapDao.getInstance().selectAll();
+        for (var ncc : armt) {
+            if (ncc.getDiaChi().toLowerCase().contains(text.toLowerCase())) {
+                result.add(ncc);
+            }
+        }
+        return result;
+    }
+
+    public ArrayList<NhaCungCap> searchSdt(String text) {
+        ArrayList<NhaCungCap> result = new ArrayList<>();
+        ArrayList<NhaCungCap> armt = NhaCungCapDao.getInstance().selectAll();
+        for (var ncc : armt) {
+            if (ncc.getSdt().toLowerCase().contains(text.toLowerCase())) {
+                result.add(ncc);
+            }
+        }
+        return result;
+    }
 }
